4b9f4195c6f0ae98266b245ffc63f6cd6376de27
Yulia Shea <yulia.shea@gmail.com>
2016-03-02 23:17:23 -0500

    project update 2

--- fallingwords_ys/main.js
+++ fallingwords_ys/main.js
@@ -3,22 +3,27 @@ console.log("JS has loaded...")
 
 var gameField = document.querySelector('.gamefield');
 
-// make words fall down
-var fallingWord = {
-  currentWord: null,
-  generateCurrentWord: function() {
+// make words and make them fall down
+
+function FactoryOfWords(currentWord,wordLeftPosition,wordNode) {
+  var newFallingWord = {};
+
+  newFallingWord.currentWord = currentWord;
+  newFallingWord.generateCurrentWord = function() {
     this.currentWord = wordArray[0]; // grabbing first word and placing it into current word
     console.log(this.currentWord);
     wordArray.push(wordArray.shift()); // moving first word to become last
     // console.log(wordArray);
-  },
-  wordLeftPosition: null,
-  generateWordPosition: function() {
+    this.generateWordPosition();
+  };
+  newFallingWord.wordLeftPosition = wordLeftPosition;
+  newFallingWord.generateWordPosition = function() {
     this.wordLeftPosition = Math.floor(Math.random() * (845 - 15)) + 15; // generate number that will determine word position
     console.log("Word position is " + this.wordLeftPosition);
-  },
-  wordNode: null,
-  generateWordNode: function() {
+    this.generateWordNode();
+  };
+  newFallingWord.wordNode = wordNode;
+  newFallingWord.generateWordNode = function() {
     var newWord = document.createElement('span');
     newWord.innerText = this.currentWord;
     newWord.style.position = "absolute";
@@ -26,12 +31,14 @@ var fallingWord = {
     newWord.style.top = "-10px"
     this.wordNode = newWord;
     console.log("Node has been generated " + this.wordNode);
-  },
-  displayCurrentWord: function() {
+    this.displayCurrentWord();
+  };
+  newFallingWord.displayCurrentWord = function() {
     gameField.appendChild(this.wordNode);
     console.log("Node has been displayed");
-  },
-  moveWordDown: function() {
+    this.moveWordDown();
+  };
+  newFallingWord.moveWordDown = function() {
     that = this.wordNode;
     var top = -10;
     var fallDown = setInterval(function() {
@@ -42,16 +49,25 @@ var fallingWord = {
     } else {
     that.classList.add('explode');
     window.clearInterval(fallDown);
-  }}, 10);
-  }
-}
-var playgame = function() {
-fallingWord.generateCurrentWord();
-fallingWord.generateWordPosition();
-fallingWord.generateWordNode();
-fallingWord.displayCurrentWord();
-fallingWord.moveWordDown();
+    }}, 10);
+  };
+  return newFallingWord;
 };
+
+
+var playgame = function() {
+    var i = wordArray.length - 1;
+      for (var i = 0; i < wordArray.lenght; i++) {
+      var intervalID = window.setTimeout(function() {
+        var newWord = FactoryOfWords();
+        newWord.generateCurrentWord();
+        // i -= 1
+        // if (i === 0) {
+        //   windown.clearInterval(intervalID);
+        // }
+      },5000)
+  };
+}
 // make words react to user input
 
 // make a player and capture actions
